053280cd1b82ca9e49f1da33a490b7e56d581853
Alex Arslan <ararslan@comcast.net>
2016-08-20 12:32:20 -0700

    Reduce to 2 containers, fix path issue

--- contrib/circle_balanceload.sh
+++ contrib/circle_balanceload.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-# Balance the testing load between 4 CircleCI parallel containers
+# Balance the testing load between 2 CircleCI parallel containers
 
-tests=($(julia -e 'include("choosetests.jl"); t = choosetests(["all"])[1]; print(join(t, " "))'))
+tests=($(/tmp/julia/bin/julia -e 'include("choosetests.jl"); t = choosetests(["all"])[1]; print(join(t, " "))'))
 
 # Command to run tests -- append names for specific tests
 jlcmd="/tmp/julia/bin/julia --check-bounds=yes runtests.jl"
 
-# There are 23 linear algebra tests. Divide them between the first two containers.
-cut1=11
-
-# That leaves 106 other tests. Try dividing them into one small and one large group.
-cut2=35
+# Cut my build into pieces, this is my last resort
+cutoff=40
 
 case $CIRCLE_NODE_INDEX in
-  0) $jlcmd ${tests[@]:0:${cut1}} ;;
-  1) $jlcmd ${tests[@]:${cut1}+1:23} ;;
-  2) $jlcmd ${tests[@]:24:${cut2}} ;;
-  3) $jlcmd ${tests[@]:${cut2}+1} ;;
+  0) $jlcmd ${tests[@]:0:${cutoff}} ;;
+  1) $jlcmd ${tests[@]:${cutoff}+1} ;;
 esac
