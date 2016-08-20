ee08e2749e652b0c0abb9cc61ea6e4e042ddc3a8
Alex Arslan <ararslan@comcast.net>
2016-08-20 12:05:33 -0700

    Make the load balancer probably worse

--- contrib/circle_balanceload.sh
+++ contrib/circle_balanceload.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# Balance the testing load between 3 CircleCI parallel containers
+# Balance the testing load between 4 CircleCI parallel containers
 
-tests=($(julia -e 'include("choosetests.jl"); t = unique(map(s -> split(s, "/")[1], choosetests(["all"])[1])); print(join(t, " "))'))
+tests=($(julia -e 'include("choosetests.jl"); t = choosetests(["all"])[1]; print(join(t, " "))'))
 
 # Command to run tests -- append names for specific tests
 jlcmd="/tmp/julia/bin/julia --check-bounds=yes runtests.jl"
 
-# The point at which we cut the tests between workers 1 and 2 (0 always gets linalg)
-cutoff=23
+# There are 23 linear algebra tests. Divide them between the first two containers.
+cut1=11
+
+# That leaves 106 other tests. Try dividing them into one small and one large group.
+cut2=35
 
 case $CIRCLE_NODE_INDEX in
-  0) $jlcmd ${tests[0]} ;;
-  1) $jlcmd ${tests[@]:1:${cutoff}} ;;
-  2) $jlcmd ${tests[@]:${cutoff}+1} fft dsp examples ;;
+  0) $jlcmd ${tests[@]:0:${cut1}} ;;
+  1) $jlcmd ${tests[@]:${cut1}+1:23} ;;
+  2) $jlcmd ${tests[@]:24:${cut2}} ;;
+  3) $jlcmd ${tests[@]:${cut2}+1} ;;
 esac
